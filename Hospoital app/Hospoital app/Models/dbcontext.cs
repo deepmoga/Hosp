94342bfa8643ff6c7d1a92745a9090c53a05fafb
deepmoga <32519872+deepmoga@users.noreply.github.com>
2017-10-05 21:33:36 +0530

    hello now i setup the project

---
+++ Hospoital app/Hospoital app/Models/dbcontext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace Admin2.Models
+{
+    public class dbcontext:DbContext
+    {
+        public dbcontext() : base("dbcontext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<dbcontext, Hospoital_app.Migrations.Configuration>("dbcontext"));
+        }
+        
+    }
+}
